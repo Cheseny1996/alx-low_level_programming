cf1c481bc912449dba996bdeae9d763d52ea3636
Cheseny1996 <eliot.cheseny@gmail.com>
2022-06-08 10:44:09 -0700

    Task 2 fixed

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,8 +2,8 @@
 #include"main.h"
 /**
  * print_alphabet_X10 -> prints the lower case alphabets,
- * followed by a new line
- * Return: Always 0 (Success)
+ * 
+ * Return: void
  */
 void print_alphabet_x10(void)
 {
