2ba5fd87090f126a93579eaa264dc6ad969fbf85
Cheseny1996 <eliot.cheseny@gmail.com>
2022-06-08 00:37:12 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  * followed by a new line
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -15,5 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return(0);
+	return (0);
 }
