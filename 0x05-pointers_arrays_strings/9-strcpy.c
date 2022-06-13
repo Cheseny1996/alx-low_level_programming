062bcea88d7e5f2b28d2e10987b75aa9912c9110
Cheseny1996 <eliot.cheseny@gmail.com>
2022-06-13 07:27:52 -0700

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,10 +1,13 @@
 #include"main.h"
 #include <stdio.h>
 
-/**_strcpy - check the code for ALX School students.
+/**
+ * _strcpy - check the code for ALX School students.
  * @dest: pointer char type
  * @src: pointer char type
- * Return: Always 0.
+ * Description: Copy the string pointed to by pointer `src` to
+ * the buffer pointed to by `dest`
+ * Return: Pointer to `dest
  */
 char *_strcpy(char *dest, char *src)
 {
