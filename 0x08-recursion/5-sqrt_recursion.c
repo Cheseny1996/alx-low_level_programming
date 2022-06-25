410710819f2d850f58fcb9d372dbad74247014a2
Cheseny1996 <eliot.cheseny@gmail.com>
2022-06-25 03:06:38 -0700

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -23,10 +23,9 @@ int _sqrt(int n, int i)
 	int square = i * i;
 
 	if (square > n)
-
 		return (-1);
-		if (square == n)
-			return (i);
-		return (_sqrt(n, i + 1));
+	if (square == n)
+		return (i);
+	return (_sqrt(n, i + 1));
 }
 
